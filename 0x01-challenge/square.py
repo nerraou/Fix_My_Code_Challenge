62abc44a43b31bf05f943d533b04e14c0991337b
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-11-16 14:38:14 -0800

    chore(square): fix style

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+"""
+square class
+"""
+
 
 class square():
-    
+    """ square class"""
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """init width and height"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,13 +19,15 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """calculate permiter"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """string representation"""
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     s = square(width=12, height=12)
     print(s)
     print(s.area_of_my_square())
